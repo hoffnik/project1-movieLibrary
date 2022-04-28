f640fd32fd1b9d66ca20d0c6d3f117e0cb797c72
hoffnik <hoffnik.dev@gmail.com>
2022-04-27 19:06:12 -0700

    new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,19 @@ var requestURL = 'https://imdb-api.com/en/API/SearchMovie/k_xuhun4lc/inception'
 
 fetch(requestURL)
 .then(function(response) {
-    return response.json();
-    console.log(response);
+    if (response.status !== 200) {
+        document.location.status.replace('./404.html')
+    } else {
+        // convert to JSON object
+        return response.json();
+        console.log(response);
+    }
 })
 .then(function(data){
+    // display data in console, change to according HTML element later
     console.log(data);
+})
+.catch(function(error) {
+    // in case error occurs
+    console.log(error);
 })
